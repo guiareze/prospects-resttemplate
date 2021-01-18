573729d7e1342e03d06438f31c3ebed665f41066
guiarese <guilhermearesende1@gmail.com>
2021-01-17 22:20:05 -0300

    first commit

---
+++ src/main/java/br/com/guiareze/prospectsresttemplate/ProspectsRestTemplateApplication.java
@@ -0,0 +1,13 @@
+package br.com.guiareze.prospectsresttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProspectsRestTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProspectsRestTemplateApplication.class, args);
+	}
+
+}
